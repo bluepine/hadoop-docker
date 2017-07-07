83f800aec5d1978925680fdad2e1ffd1462befec
Song Wei <1983.song.wei@gmail.com>
2017-07-07 17:15:45 -0400

    t

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+TAG=local/hadoop-2
 bash ./stop.sh
-#sudo rm -rf ./shared/log
-make
-#mkdir -p ./shared/log
+docker build -t $TAG --rm=true .
+bash ./clean_up_images.sh
+function extract_env {
+    docker run --rm $TAG sh -c 'echo $'${1}
+}
 #docker-compose up
+export HDFS_VOL1=`extract_env HDFS_VOL1`
+export HDFS_VOL2=`extract_env HDFS_VOL2`
 docker stack deploy --compose-file=docker-compose.yml hadoop
 
