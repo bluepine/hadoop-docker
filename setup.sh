f01398b2d936ba84e6e279798b977a16d54a75d3
Song Wei <1983.song.wei@gmail.com>
2017-07-07 17:49:58 -0400

    t

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+TAG=local/hadoop-2
 function extract_env {
     docker run --rm $TAG sh -c 'echo $'${1}
 }
-TAG=local/hadoop-2
 docker build -t $TAG --rm=true .
 bash ./clean_up_images.sh
 export HDFS_VOL1=`extract_env HDFS_VOL1`
